0ed34a5cc52b23c5c99c74363eeeacb5e7dd58ea
SaiShanky18 <151673364+SaiShanky18@users.noreply.github.com>
2024-06-03 18:56:33 -0400

    cors error highlighting

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -27,7 +27,7 @@ export const GET = async (
         return new NextResponse(JSON.stringify(product), {
             status: 200,
             headers: {
-                "Access-Control-Allow-Origin": `${process.env.ECOMMERCE_STORE_URL}`,
+                "Access-Control-Allow-Origin": `${process.env.ECOMMERCE_STORE_URL}`, //here is the cors error
                 "Access-Control-Allow-Methods": "GET",
                 "Access-Control-Allow-Headers": "Content-Type",
             },
